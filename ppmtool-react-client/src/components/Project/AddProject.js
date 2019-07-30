d78317d7137b9db84ec6a6ff0905377a300263cc
hakim arhazzal <hakim.arhazzal@gmail.com>
2019-07-30 19:33:23 +0200

    Style validation errors with classnames

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {createProject} from '../../actions/projectActions';
+import classnames from "classnames";
 
 class AddProject extends Component {
     constructor() {
@@ -60,58 +61,88 @@ class AddProject extends Component {
                       <div className="form-group">
                         <input
                           type="text"
-                          className="form-control form-control-lg"
+                          className={classnames("form-control form-control-lg", {
+                            "is-invalid": errors.projectName
+                          })}
                           placeholder="Project Name"
                           name="projectName"
                           value={this.state.projectName}
                           onChange={this.onChange}
                         />
-                        <p>{errors.projectName}</p>
+                        {errors.projectName && (
+                          <div className="invalid-feedback">
+                            {errors.projectName}
+                          </div>
+                        )}
                        
                       </div>
                       <div className="form-group">
                         <input
                           type="text"
-                          className="form-control form-control-lg"
+                          className={classnames("form-control form-control-lg", {
+                            "is-invalid": errors.projectIdentifier
+                          })}
                           placeholder="Unique Project ID"
                           name="projectIdentifier"
                           value={this.state.projectIdentifier}
                           onChange={this.onChange}
                         />
-                        <p>{errors.projectIdentifier}</p>
+                        {errors.projectName && (
+                          <div className="invalid-feedback">
+                            {errors.projectIdentifier}
+                          </div>
+                        )}
                       
                       </div>
                       <div className="form-group">
                         <textarea
                           placeholder="Project Description"
-                          className="form-control form-control-lg"
+                          className={classnames("form-control form-control-lg", {
+                            "is-invalid": errors.description
+                          })}
                           name="description"
                           value={this.state.description}
                           onChange={this.onChange}
                         />
-                        <p>{errors.description}</p>
+                        {errors.projectName && (
+                          <div className="invalid-feedback">
+                            {errors.description}
+                          </div>
+                        )}
                       </div>
                       <h6>Start Date</h6>
                       <div className="form-group">
                         <input
                           type="date"
-                          className="form-control form-control-lg"
+                          className={classnames("form-control form-control-lg", {
+                            "is-invalid": errors.start_date
+                          })}
                           name="start_date"
                           value={this.state.start_date}
                           onChange={this.onChange}
                         />
-                        <p>{errors.start_date}</p>
+                        {errors.projectName && (
+                          <div className="invalid-feedback">
+                            {errors.start_date}
+                          </div>
+                        )}
                       </div>
                       <h6>Estimated End Date</h6>
                       <div className="form-group">
                         <input
                           type="date"
-                          className="form-control form-control-lg"
+                          className={classnames("form-control form-control-lg", {
+                            "is-invalid": errors.end_date
+                          })}
                           name="end_date"
                           value={this.state.end_date}
                           onChange={this.onChange}
                         />
-                        <p>{errors.end_date}</p>
+                        {errors.projectName && (
+                          <div className="invalid-feedback">
+                            {errors.end_date}
+                          </div>
+                        )}
                       </div>
          
                       <input
