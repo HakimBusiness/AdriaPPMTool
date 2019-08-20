32446998a4b8b20f632f7caa8b8aa43a95d15593
hakim arhazzal <hakim.arhazzal@gmail.com>
2019-08-20 03:12:38 +0200

    token generated!!

--- ppmtool/src/main/java/io/adria/ppmtool/web/UserController.java
+++ ppmtool/src/main/java/io/adria/ppmtool/web/UserController.java
@@ -1,17 +1,25 @@
 package io.adria.ppmtool.web;
 
 import io.adria.ppmtool.domain.User;
+import io.adria.ppmtool.payload.JWTLoginSucessReponse;
+import io.adria.ppmtool.payload.LoginRequest;
+import io.adria.ppmtool.security.JwtTokenProvider;
 import io.adria.ppmtool.services.MapValidationErrorService;
 import io.adria.ppmtool.services.UserService;
 import io.adria.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static io.adria.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 import javax.validation.Valid;
 
@@ -26,6 +34,33 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        System.out.println(authentication);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX +  tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSucessReponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
